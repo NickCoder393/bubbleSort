c6a816ef696328caf322d79809a8936314053607
nicolas <nicolasczaikowski@gmail.com>
2022-02-02 22:34:06 -0300

    algoritmo para ordenacao

---
+++ index.js
@@ -0,0 +1,37 @@
+function bubbleSort(arrNumbers){
+	let arr =arrNumbers.sort(function(a, b){
+
+			return a - b 
+
+	});
+	console.log("hello world, ====> ", arr);
+
+}
+
+bubbleSort([10, 5, 3, 2, 6, 7, 9, 1, 4, 8]);
+
+
+
+
+function bubbleSort(arrNumbers){
+
+	for(let i = 0; i < arrNumbers.length; i++) {
+		for(let j = 0; j < arrNumbers.length; j++) {
+
+			if(arrNumbers[j] > arrNumbers[j + 1]) {
+
+				/** Inicio logica inversao **/
+				let temp = arrNumbers[ j ];
+				arrNumbers[ j ] = arrNumbers[j + 1];
+				arrNumbers[ j + 1] = temp;
+
+				/** Final logica inversao **/
+
+			}
+
+		}
+	}
+	console.log(arrNumbers);
+}
+
+bubbleSort([10, 5, 3, 2, 6, 7, 9, 1, 4, 8])
